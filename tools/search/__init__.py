256b79af0eeea26946d41fd6d8f7b33f5758bf67
Erdoschn <ydengbr@connect.ust.hk>
2026-09-17 18:12:48 +0800

    export refactored search response and HTTP types

--- tools/search/__init__.py
+++ tools/search/__init__.py
@@ -1,14 +1,20 @@
-from .models import SearchQuery, SearchResult
+from .models import SearchError, SearchQuery, SearchResponse, SearchResult
 from .base import SearchProvider
 from .arxiv import ArxivSearchProvider
 from .wikipedia import WikipediaSearchProvider
 from .router import SearchRouter
+from .http import HttpClient, HttpRequestError, HttpResponse
 
 __all__ = [
     "SearchQuery",
     "SearchResult",
+    "SearchError",
+    "SearchResponse",
     "SearchProvider",
     "ArxivSearchProvider",
     "WikipediaSearchProvider",
     "SearchRouter",
+    "HttpClient",
+    "HttpRequestError",
+    "HttpResponse",
 ]
